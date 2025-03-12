8082388906c23e0140c128d29a6b05e999255807
jinseop5 <wlstjqdlrk@gmail.com>
2025-03-12 20:59:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import pandas as pd
 from telegram import Bot
 
 # 🔹 텔레그램 봇 정보 입력 (BotFather에서 발급)
-TELEGRAM_BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
-TELEGRAM_CHAT_ID = "YOUR_CHAT_ID"
+TELEGRAM_BOT_TOKEN = "8025718450:AAHPdi-tgOhY-OqWTV8RvmN_T9betoCwpto"
+TELEGRAM_CHAT_ID = "7752168245"
 
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
